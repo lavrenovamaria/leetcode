42c29b4bbab6688ce94c78d1b396bb0cf7419999
marialavrenova <lavrenova.maria.11@gmail.com>
2023-10-16 10:31:03 +0300

    Sums

--- MySqrt.java
+++ MySqrt.java
@@ -1,11 +1,7 @@
 package leetcode;
 
-import java.util.HashMap;
-import java.util.Map;
-
 public class MySqrt {
     public int mySqrt(int n) {
-        //Babylonian method
         if(n == 0)
             return 0;
         double x = n;
@@ -16,21 +12,10 @@ public class MySqrt {
             y = n / x;
         }
         return (int)x;
-//        for(int i = 1; i <= x; i++){
-//            if(i * i == x)
-//                return i;
-//            else if(i * i > x)
-//                return i - 1;
-//        }
-//        return 0;
-
     }
 
     public static void main(String[] args) {
         MySqrt sq = new MySqrt();
         System.out.println(sq.mySqrt(16));
-//        System.out.println(sq.mySqrt(21474836));
-//        System.out.println(sq.mySqrt(2147395600));
-//        System.out.println(sq.mySqrt(2147483647));
     }
 }
